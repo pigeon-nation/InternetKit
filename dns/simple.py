c167493b5101e13ff057f7d18df853a6abb11be4
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2023-08-15 19:03:39 +1000

    First DNS Lookup Release

---
+++ dns/simple.py
@@ -0,0 +1,14 @@
+import socket
+
+class Lookup:
+	def __init__(self):
+		pass
+	
+	def lookup(self, name):
+		return socket.gethostbyname(name)
+	
+	@property
+	def name(self):
+		return socket.gethostname()
+	
+x = Lookup()
